3dd11db516fd9689b102da33e41e2d1803ad7e0c
Piotr Śródka <piotr.srodka@outlook.com>
2025-08-15 20:18:05 +0200

    ValuePLN Not Mapped + UI

--- WebApi/Database/Entities/Flow/ValueObject.cs
+++ WebApi/Database/Entities/Flow/ValueObject.cs
@@ -1,11 +1,14 @@
-﻿namespace Database.Entities
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Database.Entities
 {
-    public class ValueObject : BaseEntity
-    {
-        public string Name { get; set; } = string.Empty;
-        public decimal Value { get; set; } = 0;
-        public decimal ValuePLN { get; set; } = 0;
-        public string Currency { get; set; } = string.Empty;        
-        public string Tags { get; set; } = string.Empty;
-    }
+  public class ValueObject : BaseEntity
+  {
+    public string Name { get; set; } = string.Empty;
+    public decimal Value { get; set; } = 0;
+    [NotMapped]
+    public decimal ValuePLN { get; set; } = 0;
+    public string Currency { get; set; } = string.Empty;
+    public string Tags { get; set; } = string.Empty;
+  }
 }
---
+++ WebApi/Database/Migrations/20250815181232_NotMappedValudePLN.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    public partial class NotMappedValudePLN : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValuePLN",
+                table: "Profits");
+
+            migrationBuilder.DropColumn(
+                name: "ValuePLN",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "ValuePLN",
+                table: "Assets");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValuePLN",
+                table: "Profits",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValuePLN",
+                table: "Expenses",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValuePLN",
+                table: "Assets",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
